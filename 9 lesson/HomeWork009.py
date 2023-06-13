b2e2ff3f581305fafac49dea0d80ab59e79944b9
Damir <karmus@example.com>
2023-06-13 15:59:08 +0300

    решил домашния задания 9 урока

--- 9 lesson/HomeWork009.py
+++ 9 lesson/HomeWork009.py
@@ -4,5 +4,4 @@ data = pd.read_csv('california_housing_test.csv')
 print(data[ data['population'] < 500 ]['median_house_value'].mean())
 
 # Задача 42:
-
 print(data[data['population'] == data['population'].min()]['households'].max())
