af44e685fcf6aedafd5c200b9f2c6d8836b7e660
Damir <karmus@example.com>
2023-06-13 15:56:50 +0300

    решил домашния задания 9 урока

--- 9 lesson/HomeWork009.py
+++ 9 lesson/HomeWork009.py
@@ -1,4 +1,8 @@
-# Задача 40: Работать с файлом california_housing_train.csv, который находится в папке sample_data.
-# Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
-#
-# Задача 42: Узнать какая максимальная households в зоне минимального значения population.
+# Задача 40:
+import pandas as pd
+data = pd.read_csv('california_housing_test.csv')
+print(data[ data['population'] < 500 ]['median_house_value'].mean())
+
+# Задача 42:
+
+print(data[data['population'] == data['population'].min()]['households'].max())
